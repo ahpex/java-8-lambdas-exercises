0303e7a7fc297ddff56a8836176db2afaf3059ba
Michael Pietsch <mp@mpietsch.com>
2016-01-31 18:16:22 +0100

    solved chapter3/Question1

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/Question1.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/Question1.java
@@ -1,24 +1,22 @@
 package com.insightfullogic.java8.exercises.chapter3;
 
-import com.insightfullogic.java8.examples.chapter1.Album;
-import com.insightfullogic.java8.examples.chapter1.Artist;
-import com.insightfullogic.java8.exercises.Exercises;
-
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toList;
+import com.insightfullogic.java8.examples.chapter1.Album;
+import com.insightfullogic.java8.examples.chapter1.Artist;
 
 public class Question1 {
-    public static int addUp(Stream<Integer> numbers) {
-        return Exercises.replaceThisWithSolution();
-    }
+	public static int addUp(Stream<Integer> numbers) {
+		return numbers.reduce(0, (curr, ne) -> curr + ne).intValue();
+	}
 
-    public static List<String> getNamesAndOrigins(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
-    }
+	public static List<String> getNamesAndOrigins(List<Artist> artists) {
+		return artists.stream().flatMap(artist -> Stream.of(artist.getName(), artist.getNationality())).collect(Collectors.toList());
+	}
 
-    public static List<Album> getAlbumsWithAtMostThreeTracks(List<Album> input) {
-        return Exercises.replaceThisWithSolution();
-    }
+	public static List<Album> getAlbumsWithAtMostThreeTracks(List<Album> input) {
+		return input.stream().filter(x -> x.getTrackList().size() < 3).collect(Collectors.toList());
+	}
 }
