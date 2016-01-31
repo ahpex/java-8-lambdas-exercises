9c3545811b1472c883e76c0855a30810ea9b9a9f
Michael Pietsch <mp@mpietsch.com>
2016-01-31 18:36:10 +0100

    solved chapter3/Question2

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/Question2.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/Question2.java
@@ -1,13 +1,12 @@
 package com.insightfullogic.java8.exercises.chapter3;
 
-import com.insightfullogic.java8.examples.chapter1.Artist;
-import com.insightfullogic.java8.exercises.Exercises;
-
 import java.util.List;
 
+import com.insightfullogic.java8.examples.chapter1.Artist;
+
 public class Question2 {
-    // Q3
-    public static int countBandMembersInternal(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
-    }
+	// Q3
+	public static int countBandMembersInternal(List<Artist> artists) {
+		return artists.stream().map(artist -> artist.getMembers().count()).reduce(0L, Long::sum).intValue();
+	}
 }
